97f5d0f811becee38eedb32d0db78c530da3b138
dmezhnov <dmezhnov@greet-go.com>
2025-12-17 14:11:50 +0000

    draft-2025-12-17-14-11-50

---
+++ initial_data.example.ts
@@ -0,0 +1,8 @@
+import { MatrixBoi } from 'lib/domen/kc-e.mybpm.kz/KCE/Группа/Матрицы/Matrix.bun';
+import createMatrix from 'lib/utils/createMatrix.bun';
+
+const requests_by_period: MatrixBoi = new MatrixBoi();
+
+const volumes_by_period: MatrixBoi = new MatrixBoi();
+
+const available_by_refinery: MatrixBoi = new MatrixBoi();
