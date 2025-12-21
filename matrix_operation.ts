5501e712773146971fcdbc50f77ead7f273ffac4
dmezhnov <dmezhnov@gmail.com>
2025-12-21 19:44:34 +0500

    draft-2025-12-21-19-44-34

--- matrix_operation.ts
+++ matrix_operation.ts
@@ -4,6 +4,7 @@ import asArr from 'lib/core/custom/asArr.bun';
 import { matrix, axis, coordinate } from './matrix_types'
 import { CoordinateTypeBoi } from 'this/lib/domen/kc-e.mybpm.kz/KCE/Группа/Матрицы/CoordinateType.bun';
 import { MatrixOperationNewEmptyBoi } from 'lib/domen/kc-e.mybpm.kz/KCE/Группа/Матрицы/MatrixOperationNewEmpty.bun';
+import { MatrixOperationFilterByCoordinateBoi } from 'lib/domen/kc-e.mybpm.kz/KCE/Группа/Матрицы/MatrixOperationFilterByCoordinate.bun';
 
 import matrix_sum_by_axes_create_structure_1 from "lib/domen/kc-e.mybpm.kz/KCE/Tasks/matrix_sum_by_axes_create_structure_1.bun";
 import matrix_sum_by_axes_create_structure_2 from "lib/domen/kc-e.mybpm.kz/KCE/Tasks/matrix_sum_by_axes_create_structure_2.bun";
@@ -16,6 +17,9 @@ import create_empty_matrix_group from "lib/domen/kc-e.mybpm.kz/KCE/Tasks/create_
 import create_empty_matrix_all_el from "lib/domen/kc-e.mybpm.kz/KCE/Tasks/create_empty_matrix_all_el.bun";
 import create_empty_matrix_all_struct from "lib/domen/kc-e.mybpm.kz/KCE/Tasks/create_empty_matrix_all_struct.bun";
 import { CoordinateBo, CoordinateBoi } from 'this/lib/domen/kc-e.mybpm.kz/KCE/Группа/Матрицы/Coordinate.bun';
+
+import matrix_filter_by_coord_finalize from 'this/lib/domen/kc-e.mybpm.kz/KCE/Tasks/matrix_filter_by_coord_finalize.bun';
+import matrix_filter_by_coord_traverse from 'this/lib/domen/kc-e.mybpm.kz/KCE/Tasks/matrix_filter_by_coord_traverse.bun';
 
 const arrayToScratchFormat = <TEl>(array: TEl | asArr<TEl>): ScratchArr<TEl> => {
     //@ts-ignore
@@ -56,7 +60,23 @@ export const sum_by_axes = <
     return operation.matrix_output_1['#значение'] as matrix<TAxesAfter>;
 }
 
+export const filter_by_coordinate = <
+    TCoords extends coordinate[] = coordinate[],
+    TCoord extends coordinate = TCoords[number]
+>(matrix_input: matrix, ...coordinate_input: TCoords): matrix => {
+    const operation = new MatrixOperationFilterByCoordinateBoi();
+
+    operation.matrix_input_1['#значение'] = matrix_input;
+    operation.coordinates_input_1['#значение'] = arrayToScratchFormat(coordinate_input) as unknown as ScratchArr<TCoord & CoordinateBoi<CoordinateBo>>;
+
+    matrix_filter_by_coord_traverse.call(operation);
+    matrix_filter_by_coord_finalize.call(operation);
+
+    return operation.matrix_output_1['#значение'] as matrix;
+}
+
 export default {
     sum_by_axes,
-    new_empty
+    new_empty,
+    filter_by_coordinate
 }
--- step-1.ts
+++ step-1.ts
@@ -1,18 +1,11 @@
-import { MatrixBoi } from 'lib/domen/kc-e.mybpm.kz/KCE/Группа/Матрицы/Matrix.bun';
-import ScratchArr from 'lib/core/default/ScratchArr.bun';
-import asArr from 'lib/core/custom/asArr.bun';
+import { filter_by_coordinate, sum_by_axes } from './matrix_operation';
+import { axis, matrix } from './matrix_types';
 import { requests_by_period } from './initial_data.example'
 
+export const requests_by_period_first_queue: matrix = matrix();
 
-const arrayToScratchFormat = <TEl>(array: TEl | asArr<TEl>): ScratchArr<TEl> => {
-    //@ts-ignore
-    return new ScratchArr(array);
-}
+export const requests_by_queue: matrix = matrix();
 
-export const requests_by_period_first_queue: MatrixBoi = new MatrixBoi();
+export const requests_by_market_participant: matrix = matrix();
 
-export const requests_by_queue: MatrixBoi = new MatrixBoi();
-
-export const requests_by_market_participant: MatrixBoi = new MatrixBoi();
-
-export const requests_by_market_participant_region: MatrixBoi = new MatrixBoi();
+export const requests_by_market_participant_region: matrix = matrix();
