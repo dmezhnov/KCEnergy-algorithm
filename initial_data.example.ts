73126e4f5ba686845d225a47ef511ef018559cdf
dmezhnov <dmezhnov@gmail.com>
2025-12-22 09:30:42 +0500

    draft-2025-12-22-09-30-42

--- initial_data.example.ts
+++ initial_data.example.ts
@@ -418,7 +418,7 @@ matrixFor(requestsEmptyMatrix, (el: matrix) => {
 
 export const requests_by_period: matrix = requestsEmptyMatrix;
 
-// console.log(requests_by_period.toString());
+console.log(requests_by_period.toString());
 
 const volumesEmptyMatrix: matrix = matrix(
     ai_92,
