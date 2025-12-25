d163cdd83198678238b0029d099bd1e6f68eea13
dmezhnov <dmezhnov@gmail.com>
2025-12-25 12:22:31 +0500

    draft-2025-12-25-12-22-31

--- step-1.ts
+++ step-1.ts
@@ -9,18 +9,18 @@ export const requests_i_j_k_l_l0_first: matrix = filter_by_coordinate(requests_i
 requests_i_j_k_l_l0_first.matrix_title['#значение'] = 'requests_i_j_k_l_l0_first';
 
 
-export const requests_by_queue: matrix = sum_by_axes(requests_i_j_k_l_l0, Queue);
-requests_by_queue.matrix_title['#значение'] = 'requests_by_queue';
+export const requests_i_j_k_l: matrix = sum_by_axes(requests_i_j_k_l_l0, Queue);
+requests_i_j_k_l.matrix_title['#значение'] = 'requests_i_j_k_l';
 
-export const requests_by_market_participant: matrix = sum_by_axes(requests_by_queue, Market_participant);
-requests_by_market_participant.matrix_title['#значение'] = 'requests_by_market_participant';
+export const requests_i_j_k: matrix = sum_by_axes(requests_i_j_k_l, Market_participant);
+requests_i_j_k.matrix_title['#значение'] = 'requests_i_j_k';
 
-export const requests_by_market_participant_region: matrix = sum_by_axes(requests_by_queue, Region, Market_participant);
-requests_by_market_participant_region.matrix_title['#значение'] = 'requests_by_market_participant_region';
+export const requests_i_j: matrix = sum_by_axes(requests_i_j_k_l, Region, Market_participant);
+requests_i_j.matrix_title['#значение'] = 'requests_i_j';
 
 if (import.meta.main) {
     await requests_i_j_k_l_l0_first.save('algorithm/build/step-1.lang', false);
-    await requests_by_queue.save('algorithm/build/step-1.lang', true);
-    await requests_by_market_participant.save('algorithm/build/step-1.lang', true);
-    await requests_by_market_participant_region.save('algorithm/build/step-1.lang', true);
+    await requests_i_j_k_l.save('algorithm/build/step-1.lang', true);
+    await requests_i_j_k.save('algorithm/build/step-1.lang', true);
+    await requests_i_j.save('algorithm/build/step-1.lang', true);
 }
