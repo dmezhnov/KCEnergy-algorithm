bf2406c7f253f996a3ae62b7a9e553f3570c1964
dmezhnov <dmezhnov@gmail.com>
2025-12-26 23:16:20 +0500

    draft-2025-12-26-23-16-20

--- step-1.ts
+++ step-1.ts
@@ -1,26 +1,26 @@
 import { filter_by_coordinate, sum_by_axes } from './matrix_operation';
 import { Axis, Coordinate, matrix } from './matrix_types';
-import { requests_i_j_k_l_l0 } from './initial_data.example'
+import { requests_i_j_s_k_l_l0 } from './initial_data.example'
 
 const { First } = Coordinate;
 const { Queue, Market_participant, Region } = Axis;
 
-export const requests_i_j_k_l_s_first: matrix = filter_by_coordinate(requests_i_j_k_l_l0, First);
+export const requests_i_j_k_l_s_first: matrix = filter_by_coordinate(requests_i_j_s_k_l_l0, First);
 requests_i_j_k_l_s_first.matrix_title['#значение'] = 'requests_i_j_k_l_s_first';
 
 
-export const requests_i_j_k_l: matrix = sum_by_axes(requests_i_j_k_l_l0, Queue);
-requests_i_j_k_l.matrix_title['#значение'] = 'requests_i_j_k_l';
+export const requests_i_j_s_k_l: matrix = sum_by_axes(requests_i_j_s_k_l_l0, Queue);
+requests_i_j_s_k_l.matrix_title['#значение'] = 'requests_i_j_s_k_l';
 
-export const requests_i_j_k: matrix = sum_by_axes(requests_i_j_k_l, Market_participant);
-requests_i_j_k.matrix_title['#значение'] = 'requests_i_j_k';
+export const requests_i_j_s_k: matrix = sum_by_axes(requests_i_j_s_k_l, Market_participant);
+requests_i_j_s_k.matrix_title['#значение'] = 'requests_i_j_s_k';
 
-export const requests_i_j_s: matrix = sum_by_axes(requests_i_j_k_l, Region, Market_participant);
-requests_i_j_s.matrix_title['#значение'] = 'requests_i_j_s';
+export const requests_i_j: matrix = sum_by_axes(requests_i_j_s_k_l, Region, Market_participant);
+requests_i_j.matrix_title['#значение'] = 'requests_i_j';
 
 if (import.meta.main) {
     await requests_i_j_k_l_s_first.save('algorithm/build/step-1.lang', false);
-    await requests_i_j_k_l.save('algorithm/build/step-1.lang', true);
-    await requests_i_j_k.save('algorithm/build/step-1.lang', true);
-    await requests_i_j_s.save('algorithm/build/step-1.lang', true);
+    await requests_i_j_s_k_l.save('algorithm/build/step-1.lang', true);
+    await requests_i_j_s_k.save('algorithm/build/step-1.lang', true);
+    await requests_i_j.save('algorithm/build/step-1.lang', true);
 }
